a5149b7c0a0b13bbdc8f82dda64cb69b537569da
Daniel Castro <daniel.castro1024@outlook.com>
2023-04-04 21:08:56 -0500

    Se añade el aside del carrito

--- main.js
+++ main.js
@@ -1,12 +1,17 @@
 const menuEmail = document.getElementsByClassName('navbar-email');
 const menuHamIcon = document.getElementsByClassName('menu')
+const menuCarIcon = document.getElementsByClassName('navbar-shopping-cart')
 
 menuEmail[0].addEventListener('click', toggleDesktopMenu);
 menuHamIcon[0].addEventListener('click', toggleMobileMenu);
+menuCarIcon[0].addEventListener('click', toggleCarAside);
 
 function toggleDesktopMenu(){
     document.querySelector('.desktop-menu').classList.toggle('inactive');
 }
 function toggleMobileMenu(){
     document.querySelector('.mobile-menu').classList.toggle('inactive')
+}
+function toggleCarAside(){
+    document.querySelector('.product-detail').classList.toggle('inactive')
 }
