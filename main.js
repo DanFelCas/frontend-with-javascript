4cc7c4cc9c5bf26acfa4c88450a80085d65b02ec
Daniel Castro <daniel.castro1024@outlook.com>
2023-04-04 20:26:55 -0500

    Se añade el menu para Mobile

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 const menuEmail = document.getElementsByClassName('navbar-email');
+const menuHamIcon = document.getElementsByClassName('menu')
 
 menuEmail[0].addEventListener('click', toggleDesktopMenu);
+menuHamIcon[0].addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu(){
     document.querySelector('.desktop-menu').classList.toggle('inactive');
+}
+function toggleMobileMenu(){
+    document.querySelector('.mobile-menu').classList.toggle('inactive')
 }
